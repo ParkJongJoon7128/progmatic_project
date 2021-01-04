b28f4a1797ec7bd43edfbea9b64e379abb807ea6
ParkJongJoon <whdwnsdk8111@naver.com>
2021-01-04 23:02:43 +0900

    progmatic_project

--- static/js/magicgrid.js
+++ static/js/magicgrid.js
@@ -283,7 +283,8 @@ let magicGrid = new MagicGrid({
 });
 
 var masonrys = document.getElementsByTagName("img");
-for(let i=0; i<masonrys.lengthl;i++){
+
+for(let i=0; i<masonrys.lengthl; i++){
   masonrys[i].addEventListener('load', function() {
     magicGrid.positionItems();
   }, false);
